076eba8cdaa23cbf59d1b3ac262f2eab71ea2df5
Emanuel Miranda <emanuelmiranda932@gmail.com>
2025-07-16 14:50:35 -0300

    removendo teste de url

--- test_url.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-url = os.getenv("DATABASE_URL")
-if not url:
-    print("DATABASE_URL not set in .env file. Using default value.\n")
-
-print(url)
